bbc9e1f0d3d1c22caaf70bad6fd3e3ed12a2508e
Adrian Gilanyi <60935462+GEdrien@users.noreply.github.com>
2023-05-22 00:59:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Property / House Catalog Application
---
+++ src/main/java/progkor/catalog/propertycatalog/PropertyCatalogApplication.java
@@ -0,0 +1,16 @@
+package progkor.catalog.propertycatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starting point of the application
+ */
+@SpringBootApplication
+public class PropertyCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyCatalogApplication.class, args);
+    }
+}
+
